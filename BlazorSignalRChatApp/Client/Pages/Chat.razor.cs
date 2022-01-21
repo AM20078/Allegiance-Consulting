7ea454803854306b5d833303570c68a052b73f8e
Anri Maré <anri.mare@gmail.com>
2022-01-21 14:16:06 +0200

    Add project files.

---
+++ BlazorSignalRChatApp/Client/Pages/Chat.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorSignalRChatApp.Client.Pages
+{
+    public partial class Chat
+    {
+        [Parameter]
+        public string? username { get; set; }
+    }
+}
---
+++ BlazorSignalRChatApp/Client/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorSignalRChatApp.Client.Pages
+{
+    public partial class Index
+    {
+        private string? username;
+        private void OnClick()
+        {
+            navManager.NavigateTo($"chat/{username}");
+        }
+    }
+}
